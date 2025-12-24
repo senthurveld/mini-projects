ed57ed6bbff71f7a3b31ce4f145397f11e734fce
senthurveld <150390304+senthurveld@users.noreply.github.com.>
2025-12-24 22:34:26 +0530

    transfer

--- country-explorer/country-explorer/vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vite";
-import tailwindcss from "@tailwindcss/vite";
-export default defineConfig({
-  base : "https://senthurveld.github.io/mini-projects/country-explorer/country-explorer",
-  plugins: [tailwindcss()],
-  server: {
-    port: 3001,
-  },
-});
